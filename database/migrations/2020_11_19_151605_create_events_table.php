f97e734792a27ad64a4f08c5dff642230e37b21c
Jordan Welch <jwelch@highlandsolutions.com>
2020-11-19 09:21:21 -0600

    Event model created

---
+++ database/migrations/2020_11_19_151605_create_events_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id('EventID');
+            $table->string('EventName');
+            $table->string('EventDate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
